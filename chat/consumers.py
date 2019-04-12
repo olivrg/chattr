1e72c479ea0c839a4cf55200acaf4dae74c4299e
oli <olivrg@gmail.com>
2019-04-12 13:52:16 +0100

    finish backend

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,22 +1,77 @@
-from channels.auth import channel_session_user_from_http
+import json
 
+from channels import Channel
+from channels.auth import channel_session_user, channel_session_user_from_http
+
+from .exceptions import ClientError
 from .models import Room
-# This decorator copies the user from the HTTP session (only available in
-# websocket.connect or http.request messages) to the channel session (available
-# in all consumers with the same reply_channel, so all three here)
+from .settings import (MSG_TYPE_ENTER, MSG_TYPE_LEAVE,
+                       NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS)
+from .utils import catch_client_error, get_room_or_error
+
+
 @channel_session_user_from_http
 def ws_connect(message):
-    message.reply_channel.send({"accept": True})
     message.channel_session['rooms'] = []
 
 
-@channel_session_user 
+def ws_receive(message):
+    payload = json.loads(message['text'])
+    payload['reply_channel'] = message.content['reply_channel']
+    Channel("chat.receive").send(payload)
+
+
+@channel_session_user
 def ws_disconnect(message):
-    # unsubscribe from any connected rooms
     for room_id in message.channel_session.get("rooms", set()):
         try:
             room = Room.objects.get(pk=room_id)
-
             room.websocket_group.discard(message.reply_channel)
         except Room.DoesNotExist:
             pass
+
+
+@channel_session_user
+@catch_client_error
+def chat_join(message):
+    room = get_room_or_error(message["room"], message.user)
+
+    if NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS:
+        room.send_message(None, message.user, MSG_TYPE_ENTER)
+
+    room.websocket_group.add(message.reply_channel)
+    message.channel_session['rooms'] = list(
+        set(message.channel_session['rooms']).union([room.id]))
+    message.reply_channel.send({
+        "text": json.dumps({
+            "join": str(room.id),
+            "title": room.title,
+        }),
+    })
+
+
+@channel_session_user
+@catch_client_error
+def chat_leave(message):
+    room = get_room_or_error(message["room"], message.user)
+
+    if NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS:
+        room.send_message(None, message.user, MSG_TYPE_LEAVE)
+
+    room.websocket_group.discard(message.reply_channel)
+    message.channel_session['rooms'] = list(
+        set(message.channel_session['rooms']).difference([room.id]))
+    message.reply_channel.send({
+        "text": json.dumps({
+            "leave": str(room.id),
+        }),
+    })
+
+
+@channel_session_user
+@catch_client_error
+def chat_send(message):
+    if int(message['room']) not in message.channel_session['rooms']:
+        raise ClientError("ROOM_ACCESS_DENIED")
+    room = get_room_or_error(message["room"], message.user)
+    room.send_message(message["message"], message.user)
--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,9 @@
+import json
+
 from channels import Group
 from django.db import models
+
+from .settings import MSG_TYPE_MESSAGE
 
 
 class Room(models.Model):
@@ -20,3 +24,15 @@ class Room(models.Model):
         messages as they are generated
         """
         return Group("room-%s" % self.id)
+
+    def send_message(self, message, user, msg_type=MSG_TYPE_MESSAGE):
+        """
+        Called to send a message to the room on behalf of a user.
+        """
+        final_msg = {'room': str(self.id), 'message': message,
+                     "username": user.username, 'msg_type': msg_type}
+
+        # Send out the message to everyone in the room
+        self.websocket_group.send(
+            {"text": json.dumps(final_msg)}
+        )
